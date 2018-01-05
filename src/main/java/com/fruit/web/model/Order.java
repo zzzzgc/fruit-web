5f4c039d1796590fa241da4e9d0505848d21d810
partnerccz <2779017796@qq.com>
2018-01-05 16:57:46 +0800

    由于Order表单字段分离出一个表，所以重新改变SQL语句

--- src/main/java/com/fruit/web/model/Order.java
+++ src/main/java/com/fruit/web/model/Order.java
@@ -15,17 +15,18 @@ public class Order extends BaseOrder<Order> {
 	// 获取我的订单数据
 	public List<Order> getOrderListByStatus(String uid, String status){
 		StringBuilder sql = new StringBuilder();
-		sql.append("select o.id,o.order_id , o.create_time , p.`name`,pi.img_url, ");
-		sql.append("if(p.country='中国','','进口') as country ,o.num, ");
-		sql.append("o.buy_remark,if(p.country='中国',false,false) `check`,o.pay_status ispay,o.sell_price,o.product_standard_name ");
-		sql.append("from b_product p,b_product_standard ps,b_order o,b_product_img pi ");
-		sql.append("where p.id=ps.product_id and p.id=o.product_id and pi.product_id=o.product_id ");
-		sql.append("and o.product_standard_id=ps.id ");
-		sql.append("and o.buy_uid=? ");
-		sql.append("and o.`status` in (");
+		sql.append("select od.id,od.order_id , od.create_time , p.`name`,pi.img_url, ");
+		sql.append("if(p.country='中国','','进口') as country ,od.num, ");
+		sql.append("od.buy_remark,if(p.country='中国',false,false) `check`,o.pay_status ispay, ");
+		sql.append("od.sell_price,od.product_standard_name ,od.`status` ");
+		sql.append("from b_product p,b_product_standard ps,b_order_detail od,b_product_img pi,b_order o ");
+		sql.append("where p.id=ps.product_id and p.id=od.product_id and pi.product_id=od.product_id ");
+		sql.append("and od.product_standard_id=ps.id  and o.order_id=od.order_id ");
+		sql.append("and od.buy_uid=? ");
+		sql.append("and od.`status` in (");
 		sql.append(status);
 		sql.append(") ");
-		sql.append("order by o.create_time ");
+		sql.append("order by od.create_time ");
 		return find(sql.toString(),uid);
 	}
 
