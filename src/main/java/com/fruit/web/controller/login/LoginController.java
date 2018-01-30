e9beea183fd3cdba3bde48b3ff80ae77854bef5c
zzzzgc <1098766713@qq.com>
2018-01-30 15:48:07 +0800

    添加验证码校验

--- src/main/java/com/fruit/web/controller/login/LoginController.java
+++ src/main/java/com/fruit/web/controller/login/LoginController.java
@@ -4,13 +4,19 @@ import com.fruit.web.base.BaseController;
 import com.fruit.web.model.BusinessUser;
 import com.fruit.web.util.Constant;
 import com.fruit.web.util.DataResult;
+import com.fruit.web.util.VerifyCodeUtils;
 import com.jfinal.kit.HashKit;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
+
 public class LoginController extends BaseController {
 
     private Logger log = Logger.getLogger(getClass());
+
+    private static int counter = 0;
 
     /**
      * 登录用的session的key
@@ -27,24 +33,14 @@ public class LoginController extends BaseController {
             //return;
         }
 
-        // TODO 用验证码作比较
-        String verifyCode = getVerifyCode();
-        if (verifyCode != null && verifyCode.equals("验证码")) {
-
-        }else {
-            renderErrorText("验证码错误!");
-            return;
-        }
-
-
         String phone = getPara("phone");
         String password = StringUtils.isNotBlank(getPara("password")) ? HashKit.md5(getPara("password")) : getPara("password");
 
         if (StringUtils.isNotBlank(phone) && StringUtils.isNotBlank(password)) {
             BusinessUser user = BusinessUser.dao.getUser(phone, password);
             if (user != null) {
-                setSessionAttr(Constant.SESSION_UID,user.getId());
-                log.info("----------session获取jsessionid为"+getSession().getId()+"----------");
+                setSessionAttr(Constant.SESSION_UID, user.getId());
+                log.info("----------session获取jsessionid为" + getSession().getId() + "----------");
                 renderNull();
             } else {
                 renderErrorText("用户名或密码有误");
@@ -63,6 +59,19 @@ public class LoginController extends BaseController {
 
     }
 
+    /**
+     * 验证码校验
+     */
+    public void validationVerifyCode() {
+        String verifyCode = getPara("verifyCode");
+        String serviceVerifyCode = getVerifyCode();
+        if (verifyCode != null && serviceVerifyCode != null && verifyCode.equals(serviceVerifyCode)) {
+
+        } else {
+            renderErrorText("验证码错误!");
+        }
+
+    }
 
     /**
      * 退出登录操作
@@ -79,16 +88,34 @@ public class LoginController extends BaseController {
     /**
      * 生成验证码
      */
-    public void createVerifyCode(){
-        //TODO 添加验证码到session和生成图片到制定目录
-        getSession().setAttribute(LOGIN_VERIFY_CODE,"");
+    public void createVerifyCode() {
+        try {
+            //文件夹
+            String folder = "/verify";
+            //文件名
+            String dirName = System.currentTimeMillis() + ++counter + ".jpg";
+            //文件路径
+            String realPath = getRequest().getSession().getServletContext().getRealPath(folder);
+
+            File dir = new File(realPath);
+            String verify = VerifyCodeUtils.generateVerifyCode(4);
+            getSession().setAttribute(LOGIN_VERIFY_CODE, verify);
+
+            File file = new File(dir, dirName);
+            VerifyCodeUtils.outputImage(200, 80, file, verify);
+            // /verify/imgName.jsp
+            renderText(folder + File.separator + dirName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * 获取来验证码
+     *
      * @return
      */
-    private String getVerifyCode(){
+    private String getVerifyCode() {
         return getSessionAttr(LOGIN_VERIFY_CODE);
     }
 
